3b1bf87d0772baf12c5a3b37930ab773627b6b01
Aleksandra <aleksandra.krisane@gmail.com>
2022-02-21 18:00:01 +0200

    day1_classwork

---
+++ day1_classwork.py
@@ -0,0 +1,23 @@
+# Write the following code in Python:
+#
+# print your name 5 times
+# print Bananananana using "Ba" once and "na" 5 times
+# print the number of seconds in a year calculated from months, days, minutes, seconds
+# how much a googol is that is 10 to the power of the 100th
+# how much will your 1000 Euro investment be worth after 12 years of 6% yearly interest?
+
+# print your name 5 times
+print('Aleksandra\n' * 5)
+
+# print Bananananana using "Ba" once and "na" 5 times
+print('Ba' + 'na' * 5)
+
+# print the number of seconds in a year calculated from months, days, minutes, seconds (this year = 365 days)
+print('number of seconds in a year calculated from months, days, minutes, seconds:')
+print(60*60*24*365)
+
+# how much a googol is that is 10 to the power of the 100th
+print(10**100)
+
+# how much will your 1000 Euro investment be worth after 12 years of 6% yearly interest?
+print(round(1000 * 1.06**12, 3))

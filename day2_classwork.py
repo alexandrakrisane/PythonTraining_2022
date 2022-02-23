9011033fb70f7118bf0c26e834cd8a4e473596b4
Aleksandra <aleksandra.krisane@gmail.com>
2022-02-23 17:04:32 +0200

    day 2 classwork

--- day2_classwork.py
+++ day2_classwork.py
@@ -4,9 +4,7 @@ import datetime
 print("___________Exercise 1 ________________")
 usr = input("Enter your username: ")
 print(f"username {usr} is saved ")
-
 age = int(input(f"Enter your age in full years, {usr}: "))
-
 print(f"{usr}'s age is {age} full years")
 
 century = 100
@@ -25,5 +23,19 @@ currentYear = datetime.datetime.now().year
 century_old2 = currentYear + till_hundred
 print('{} will be 100 years old in the year of {}'.format(usr, century_old2))
 
+
+
+print("\n___________Exercise 2 ________________")
+
+width = float(input(f"Enter width of the room in meters: "))
+length = float(input(f"Enter length of the room in meters: "))
+height = float(input(f"Enter height of the room in meters: "))
+# Right Rectangular Prism	V=LWH
+room_volume = round((width * length * height),2)
+print(f"Room volume is {room_volume}m\u00B3\n")
+
+# or:
+room_volume2 = width * length * height
+print('Room volume is {:.2f}m\u00B3'.format(room_volume2))
 
 

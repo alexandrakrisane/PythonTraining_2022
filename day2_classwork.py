d7714d796365fe7e39a7b59b6d7ddad4ede4f2fa
Aleksandra <aleksandra.krisane@gmail.com>
2022-02-23 16:49:42 +0200

    day 2 classwork

--- day2_classwork.py
+++ day2_classwork.py
@@ -1,1 +1,29 @@
 # 23.02.2022
+import datetime
+
+print("___________Exercise 1 ________________")
+usr = input("Enter your username: ")
+print(f"username {usr} is saved ")
+
+age = int(input(f"Enter your age in full years, {usr}: "))
+
+print(f"{usr}'s age is {age} full years")
+
+century = 100
+till_hundred = century - age
+
+if till_hundred > 0:
+    print(f"{usr} will be 100 years old in {till_hundred} years")
+else:
+    print(f"{usr} is already at least {century} years old")
+
+this_year = int(2022)
+century_old1 = this_year + till_hundred
+print(f"{usr} will be 100 years old in the year of {century_old1}")
+
+currentYear = datetime.datetime.now().year
+century_old2 = currentYear + till_hundred
+print('{} will be 100 years old in the year of {}'.format(usr, century_old2))
+
+
+

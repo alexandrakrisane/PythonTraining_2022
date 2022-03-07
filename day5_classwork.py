0ebb7316b9bce65fc7041146ddc116ad4b810bfb
Aleksandra <aleksandra.krisane@gmail.com>
2022-03-07 17:14:15 +0200

    day 5 classwork

---
+++ day5_classwork.py
@@ -0,0 +1,13 @@
+# 07.02.2022
+#import string
+
+
+print("\n___________Exercise 1 ________________\n")
+name = input("Input your name: ").lower()
+extra_text = ' , a thorough mess is it not '
+reversed_name = name[::-1]
+print(reversed_name.title() + extra_text + name[0].upper() + "?")
+
+
+
+

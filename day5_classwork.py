fb88c9aee75f941abd8b55510168b4711c78fa50
Aleksandra <aleksandra.krisane@gmail.com>
2022-03-07 17:33:30 +0200

    day 5 classwork

--- day5_classwork.py
+++ day5_classwork.py
@@ -7,7 +7,7 @@ print("\n___________Exercise 1 ________________\n")
 name = input("Input your name: ").lower()
 extra_text = ' , a thorough mess is it not '
 reversed_name = name[::-1]
-print(reversed_name.title() + extra_text + name[0].upper() + "?")
+print(reversed_name.capitalize() + extra_text + name[0].upper() + "?")
 
 
 

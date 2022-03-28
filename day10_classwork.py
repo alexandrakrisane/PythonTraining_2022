65871740c20b3a44d9fbda624104efd72c6b4c56
Aleksandra <aleksandra.krisane@gmail.com>
2022-03-28 17:30:05 +0300

    day 10 classwork

---
+++ day10_classwork.py
@@ -0,0 +1,55 @@
+# can be put inside the class or it's better to make _print_lyrics static inside the class?
+def print_lines(lyrics, line_count=-1):
+    all_lines_count = len(lyrics)
+    if line_count == -1:
+        line_count = len(lyrics)
+    elif line_count <= 0:
+        print("no lines to print")
+    elif all_lines_count < line_count:
+        print(f"only {all_lines_count} lines can be printed:\n")
+    for i in lyrics[:line_count]:
+        print(i)
+
+
+class Song:
+    def __init__(self, title, author, lyrics):
+        self.title = title
+        self.author = author
+        self.lyrics = lyrics
+        if title == '':
+            title = 'Unknown'
+        if author == '':
+            author = 'Unknown'
+        print(f"\n\nNew song made:\nTitle: {title} \nAuthor: {author}")
+
+    def sing(self, lines_present=-1):
+        x = '_' * (len(self.author + self.title) + 3)
+        print(x, '\nSinging:')
+        print_lines(self.lyrics, lines_present)
+        return self
+
+    def yell(self, lines_present=-1):
+        x = '_' * (len(self.author + self.title) + 3)
+        lines_upper = [line.upper() for line in self.lyrics]
+        print(x, '\nYELLING:')
+        print_lines(lines_upper, lines_present)
+        return self
+
+
+class Rap(Song):
+    def break_it(self, lines_present=-1, drop="yeah"):
+        x = '_' * (len(self.author + self.title) + 3)
+        lyrics = [line.replace(' ', f' {drop.upper()} ') + ' ' + drop.upper() for line in self.lyrics]
+        print(x, '\nRapping:')
+        print_lines(lyrics, lines_present)
+        return self
+
+
+ziemelmeita = Song('Ziemeļmeita', 'Jumprava', ['Gāju meklēt ziemeļmeitu', 'Garu, tālu ceļu veicu'])
+
+ziemelmeita.sing(1).yell(10).sing().sing(-3)
+
+
+zrap = Rap("Ziemeļmeita", "Jumprava", ["Gāju meklēt ziemeļmeitu", "Garu, tālu ceļu veicu"])
+
+zrap.break_it(1, "yah")
